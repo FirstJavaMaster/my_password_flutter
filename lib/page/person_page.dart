0c57dc849aa347afd0d4db4d3707aa3cd9a4bfe1
matrix <rick2morty@163.com>
2021-05-20 14:41:41 +0800

    底部状态栏

---
+++ lib/page/person_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PersonPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("person"),
+    );
+  }
+}
