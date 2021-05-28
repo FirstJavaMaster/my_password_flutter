47a80306f2c022c596e8901cbb5b695cb6fdfe8d
matrix <rick2morty@163.com>
2021-05-28 16:01:10 +0800

    关系选择对话框支持筛选;

--- lib/page/account_relation_list.dart
+++ lib/page/account_relation_list.dart
@@ -23,6 +23,9 @@ class AccountRelationListState extends State<AccountRelationList> {
   // 现有的account列表
   List<Account> accountList = [];
 
+  // 已过滤的account列表
+  List<Account> accountListFilter = [];
+
   // 已关联的 account 列表
   List<AccountRelation> relationList = [];
 
@@ -89,6 +92,7 @@ class AccountRelationListState extends State<AccountRelationList> {
                 textScaleFactor: 1.1,
               ),
               onPressed: () {
+                _filterAccountList();
                 _showDialogOfAccountPick();
               },
             ),
@@ -100,49 +104,54 @@ class AccountRelationListState extends State<AccountRelationList> {
   }
 
   void _showDialogOfAccountPick() {
-    _filterAccountList().then((accountListFilter) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return Dialog(
-            child: Column(
-              children: [
-                ListTile(title: () {
-                  return TextButton(
-                    child: Text(keyword.isEmpty ? '点我筛选 >' : '筛选条件【$keyword】 >'),
-                    onPressed: () {
-                      _showDialogOfKeywordPick();
-                    },
-                  );
-                }()),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: accountListFilter.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      var account = accountListFilter[index];
-                      return ListTile(
-                        title: Text(account.id.toString() + '、' + account.site_name),
-                        onTap: () => Navigator.of(context).pop(account.id),
-                      );
-                    },
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setStateOfAccountPickDialog) {
+            return Dialog(
+              child: Column(
+                children: [
+                  ListTile(title: () {
+                    return TextButton(
+                      child: Text(keyword.isEmpty ? '点击筛选 >' : '筛选条件【$keyword】'),
+                      onPressed: () {
+                        _showDialogOfKeywordPick(setStateOfAccountPickDialog);
+                      },
+                    );
+                  }()),
+                  Expanded(
+                    child: this.accountListFilter.isEmpty
+                        ? Text('~ 空空如也 ~')
+                        : ListView.separated(
+                            itemCount: accountListFilter.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              var account = accountListFilter[index];
+                              return ListTile(
+                                title: Text(account.site_name + ' - ' + account.user_name),
+                                onTap: () => Navigator.of(context).pop(account.id),
+                              );
+                            },
+                            separatorBuilder: (BuildContext context, int index) => Divider(),
+                          ),
                   ),
-                ),
-              ],
-            ),
-          );
-        },
-      ).then((targetId) {
-        if (targetId == null) {
-          return;
-        }
-        // 添加关联关系
-        var relation = AccountRelation(null, sourceId, targetId, '');
-        DatabaseUtils.getDatabase().then((db) {
-          db.accountRelationDao.add(relation).then((id) {
-            setState(() {
-              relation.id = id;
-              relationList.add(relation);
-            });
+                ],
+              ),
+            );
+          },
+        );
+      },
+    ).then((targetId) {
+      if (targetId == null) {
+        return;
+      }
+      // 添加关联关系
+      var relation = AccountRelation(null, sourceId, targetId, '');
+      DatabaseUtils.getDatabase().then((db) {
+        db.accountRelationDao.add(relation).then((id) {
+          setState(() {
+            relation.id = id;
+            relationList.add(relation);
           });
         });
       });
@@ -150,38 +159,66 @@ class AccountRelationListState extends State<AccountRelationList> {
   }
 
   // 过滤account列表
-  Future<List<Account>> _filterAccountList() async {
+  void _filterAccountList() {
     var excludeIdSet = relationList.map((e) => e.target_id).toSet();
     excludeIdSet.add(sourceId);
 
-    return accountList.where((account) {
+    var accountListFilter = accountList.where((account) {
       if (excludeIdSet.contains(account.id)) {
         return false;
       }
       if (keyword.isEmpty) {
         return true;
       }
+      var filterField = account.site_name;
       if (keyword == Constants.keywordNo) {
-        return RegExp(r'[0-9]').hasMatch(account.site_pin_yin_name);
+        return RegExp(r'[0-9]').hasMatch(filterField);
       } else {
-        return account.site_pin_yin_name.startsWith(keyword.toUpperCase()) || account.site_pin_yin_name.startsWith(keyword.toLowerCase());
+        return filterField.startsWith(keyword.toUpperCase()) || filterField.startsWith(keyword.toLowerCase());
       }
     }).toList();
+    setState(() {
+      this.accountListFilter = accountListFilter;
+    });
   }
 
-  void _showDialogOfKeywordPick() {
+  void _showDialogOfKeywordPick(StateSetter setStateOfAccountPickDialog) {
     showDialog(
-        context: context,
-        builder: (context) {
-          return Dialog(
-            child: Column(
-              children: [
-                ListTile(title: Text('选择keyword')),
-                Expanded(child: Text('abc')),
-              ],
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text('选择', textAlign: TextAlign.center),
+          children: [
+            Wrap(
+              alignment: WrapAlignment.center,
+              children: () {
+                return Constants.keywordList.map((keyword) {
+                  return TextButton(
+                    child: Text(
+                      keyword,
+                      textScaleFactor: 1.2,
+                    ),
+                    onPressed: () => Navigator.of(context).pop(keyword),
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(Size(1, 1)),
+                      padding: MaterialStateProperty.all(EdgeInsets.all(0)),
+                    ),
+                  );
+                }).toList();
+              }(),
             ),
-          );
-        }).then((value) => print(value));
+          ],
+        );
+      },
+    ).then((value) {
+      if (value == null) {
+        return;
+      }
+      setStateOfAccountPickDialog(() {
+        this.keyword = value;
+        _filterAccountList();
+      });
+    });
   }
 
   void _deleteRelation(AccountRelation relation) {
