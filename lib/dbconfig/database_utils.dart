af3d38173be890a893a2c8fd0011118637d74be4
matrix <rick2morty@163.com>
2021-06-07 16:24:12 +0800

    调整基本信息的样式;

--- lib/dbconfig/database_utils.dart
+++ lib/dbconfig/database_utils.dart
@@ -1,9 +1,18 @@
+import 'package:floor/floor.dart';
+
 import 'database.dart';
 
 final String dbFileName = "app_database.db";
 
 class DatabaseUtils {
+  // 数据库升级需要制定升级脚本. https://floor.codes/migrations/
+  static final List<Migration> migrationList = [
+    // Migration(1, 2, (database) async {
+    //   await database.execute('CREATE TABLE IF NOT EXISTS `User` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL)');
+    // }),
+  ];
+
   static Future<AppDatabase> getDatabase() async {
-    return await $FloorAppDatabase.databaseBuilder(dbFileName).build();
+    return await $FloorAppDatabase.databaseBuilder(dbFileName).addMigrations(migrationList).build();
   }
 }
